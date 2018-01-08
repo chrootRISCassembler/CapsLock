11190e9cd63ac90640e717f3415ef66b4347638f
RISCassembler <coffee5.56natosystemcall@gmail.com>
2018-01-09 01:24:15 +0900

    made the test more flexible

--- test/TestValidJSON.java
+++ test/TestValidJSON.java
@@ -1,21 +1,54 @@
 import game_info.GameEntry;
 import game_info.JSONDBReader;
 import org.testng.Assert;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class TestValidJSON {
-    private static final Path DB_PATH = Paths.get("./valid.json");
-
-    private JSONDBReader reader = new JSONDBReader(DB_PATH);
+    private Path JSONPath;
+    private JSONDBReader reader;
     private List<GameEntry> entryList;
     private GameEntry entry;
 
+    @BeforeGroups(groups = {"normal"})
+    @Parameters("JSONFilePathString")
+    public void preparePath(String JSONFilePathString){
+        JSONPath = Paths.get(JSONFilePathString);
+
+        if(Files.isReadable(JSONPath)){
+            System.out.println("OK, \"" + JSONPath + "\" is readable.");
+            System.out.println("Preparation of the test succeeded.");
+            return;
+        }
+
+        if(Files.isRegularFile(JSONPath)){
+            System.out.println(JSONPath + " is a regular file. But cannot read.");
+            System.out.println("Check the permission or the owner of the file.");
+        }
+
+        if(Files.isDirectory(JSONPath)){
+            System.out.println(JSONPath + " is a directory.");
+            System.out.println("You specified the wrong path.");
+        }
+
+        if(!Files.exists(JSONPath)){
+            System.out.println("There is no such file : " + JSONPath);
+            System.out.println("You specified the wrong path.");
+        }
+
+        System.err.println("Unable to open " + JSONPath);
+    }
+
+
     @Test(groups = {"normal"})
     public void loadJSONDB(){
+        reader = new JSONDBReader(JSONPath);
         Assert.assertTrue(reader.isLoadedFine());
         System.out.println(reader.getDocCount());
     }
