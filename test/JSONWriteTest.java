0083e23c83b8b4f6fe8528a109dcd40d1db169e6
RISCassembler <coffee5.56natosystemcall@gmail.com>
2018-02-15 19:13:16 +0900

    add test & test class for JSONDBWriter class

---
+++ test/JSONWriteTest.java
@@ -0,0 +1,43 @@
+import file_checker.FileChecker;
+import game_info.GameInfoBuilder;
+import game_info.GameRecord;
+import game_info.JSONDBWriter;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class JSONWriteTest {
+    private Path outputFile;
+
+    @Parameters({"file"})
+    JSONWriteTest(String file) {
+        outputFile = new FileChecker(file)
+                .onNotExists(dummy -> {
+                    System.err.println(dummy + " is not exist");
+                    return true;
+                })
+                .check()
+                .get();
+    }
+
+    @Test
+    void writeOneGame() throws IOException {
+        final GameRecord record = new GameInfoBuilder()
+                .setExe(Paths.get("dummyExe"))
+                .buildGameRecord();
+
+        final JSONDBWriter writer = new JSONDBWriter(outputFile)
+                .add(record);
+
+        try {
+            writer.flush();
+        }catch (IOException ex){
+            System.err.println("Failed to write game record.");
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+}
