04d5f85a364be246dfb28668c6852ad9953c7ad4
RISCassembler <coffee5.56natosystemcall@gmail.com>
2018-03-09 14:21:53 +0900

    adapt JSONDBWriter to Game

--- src/capslock/game_info/JSONDBWriter.java
+++ src/capslock/game_info/JSONDBWriter.java
@@ -1,60 +1,65 @@
 /*
-    This file is part of CapsLock. CapsLock is a simple game launcher.
-    Copyright (C) 2018 RISCassembler.
+    Copyright (C) 2018 RISCassembler
 
-    CapsLock is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Foobar is distributed in the hope that it will be useful,
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 package capslock.game_info;
 
+import methg.commonlib.file_checker.FileChecker;
 import org.json.JSONArray;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
-public class JSONDBWriter {
-    private JSONArray games = new JSONArray();
+/**
+ * ゲームの登録情報を受け取ってJSON形式でファイルに書き込む
+ */
+public final class JSONDBWriter {
+    private final JSONArray gameList = new JSONArray();
     private final Path filePath;
 
+    /**
+     * 書き込み先ファイルパスを指定してインスタンスを作る.
+     * @throws IllegalArgumentException 引数のファイルが書き込み用に開けないとき
+     */
     public JSONDBWriter(Path filePath) throws IllegalArgumentException {
-
-        if(!Files.isWritable(filePath)){
-            if(Files.isRegularFile(filePath)){
-                throw new IllegalArgumentException(filePath + " is a regular file. But cannot write.");
-            }
-
-            if(Files.isDirectory(filePath)){
-                throw new IllegalArgumentException(filePath + " is a directory.");
-            }
-
-            if(!Files.exists(filePath)){
-                throw new IllegalArgumentException("There is no such file : " + filePath);
-            }
-        }
-        this.filePath = filePath;
+        this.filePath = new FileChecker(filePath)
+                .onNotExists(dummy -> true)
+                .onCannotRead(dummy -> true)
+                .onCanExec(dummy -> true)
+                .check()
+                .orElseThrow(IllegalArgumentException::new);
     }
 
-    public JSONDBWriter add(GameRecord record){
-        games.put(record.getJSON());
+    /**
+     * 書き込むゲーム情報を追加する.
+     * @param game 追加するゲーム情報
+     */
+    public final JSONDBWriter add(Game game){
+        gameList.put(game.toJSON());
         return this;
     }
 
-    public void flush() throws IOException{
-        try(final BufferedWriter writer = Files.newBufferedWriter(filePath)){
-            games.write(writer);
+    /**
+     * {@link #add(Game)}で追加した全てのゲーム情報をファイルに書き込む.
+     * @throws IOException ファイル書き込み中にエラーが発生した.
+     */
+    public final void flush() throws IOException{
+        try(final BufferedWriter writer = Files.newBufferedWriter(filePath, StandardOpenOption.CREATE)){
+            gameList.write(writer);
         } catch (IOException ex) {
             System.err.println("Failed to write on " + filePath);
             throw ex;
