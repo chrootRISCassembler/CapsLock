fc0638d71554bbd35992bd0751c691425865348e
RISCassembler <coffee5.56natosystemcall@gmail.com>
2018-04-18 22:58:10 +0900

    Fix NullDevice#getAsFile

--- src/main/java/capslock/capslock/os_absorbing/NullDevice.java
+++ src/main/java/capslock/capslock/os_absorbing/NullDevice.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class NullDevice {
     static public File getAsFile(){
-        return System.getProperty("os.name").toLowerCase().equals("windows") ?
+        return System.getProperty("os.name").toLowerCase().startsWith("windows") ?
                 new File("nul") : new File("/dev/null");
     }
 }
